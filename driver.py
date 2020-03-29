03043fa4574a9226c0a07c702f595b257ccb4590
randypalusz <ibanezguy998@gmail.com>
2020-03-29 02:19:18 -0400

    started gamestop

--- driver.py
+++ driver.py
@@ -2,17 +2,25 @@ import requests
 import numpy as np
 from bs4 import BeautifulSoup
 from selenium import webdriver
+import dump
 
 oos_strings = {"not available", "check stores", "sold out", "see all buying options"}
-header = {'User-Agent':  'Chrome/57.0.2987.110 '}
-headerbb = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'}
-target = ["https://www.target.com/p/nintendo-switch-with-neon-blue-and-neon-red-joy-con/-/A-77464001",
-          "https://www.target.com/p/nintendo-switch-with-gray-joy-con/-/A-77464002"]
-best_buy = ["https://www.bestbuy.com/site/nintendo-switch-32gb-console-gray-joy-con/6364253.p?skuId=6364253",
-            "https://www.bestbuy.com/site/nintendo-switch-32gb-console-neon-red-neon-blue-joy-con/6364255.p?skuId=6364255"]
-amazon = ["https://www.amazon.com/Nintendo-Switch-Neon-Blue-Joy%E2%80%91/dp/B07VGRJDFY/ref=sr_1_2",
+header = {'User-Agent': 'Chrome/57.0.2987.110 '}
+headerbb = {'User-Agent':   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                            'AppleWebKit/537.36 (KHTML, like Gecko) '
+                            'Chrome/76.0.3809.132 Safari/537.36'}
+headergs = {'User-Agent':   'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) '
+                            'AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.4 '
+                            'Safari/605.1.15'}
+target = {"https://www.target.com/p/nintendo-switch-with-neon-blue-and-neon-red-joy-con/-/A-77464001",
+          "https://www.target.com/p/nintendo-switch-with-gray-joy-con/-/A-77464002"}
+best_buy = {"https://www.bestbuy.com/site/nintendo-switch-32gb-console-gray-joy-con/6364253.p?skuId=6364253",
+            "https://www.bestbuy.com/site/nintendo-switch-32gb-console-neon-red-neon-blue-joy-con/6364255.p?skuId=6364255"}
+amazon = {"https://www.amazon.com/Nintendo-Switch-Neon-Blue-Joy%E2%80%91/dp/B07VGRJDFY/ref=sr_1_2",
           "https://www.amazon.com/Nintendo-Switch-Gray-Joy%E2%80%91-HAC-001/dp/B07VJRZ62R/ref=sr_1_2",
-          "https://www.amazon.com/Nintendo-Switch-Animal-Crossing-New-Horizons/dp/B084DDDNRP/ref=sr_1_5"]
+          "https://www.amazon.com/Nintendo-Switch-Animal-Crossing-New-Horizons/dp/B084DDDNRP/ref=sr_1_5"}
+gamestop = {"https://www.gamestop.com",
+            "https://www.gamestop.com/video-games/switch/consoles/products/nintendo-switch-with-neon-blue-and-neon-red-joy-con/11095819.html"}
 
 
 def get_status(address, tag, attr, value, headers=False):
@@ -20,12 +28,14 @@ def get_status(address, tag, attr, value, headers=False):
         # use specific header for bestbuy
         if "bestbuy" in address:
             get_request = requests.get(address, headers=headerbb).text
+        elif "gamestop" in address:
+            get_request = requests.get(address, headers=dump.headergs).text
         else:
             get_request = requests.get(address, headers=header).text
     else:
         get_request = requests.get(address).text
     doc = BeautifulSoup(get_request, 'html.parser')
-    # print(doc.prettify())
+    print(doc.prettify())
     rows = doc.find_all(tag, {attr: value}, limit=1)
     for row in rows:
         stock = row.getText().strip().lower()
@@ -45,5 +55,5 @@ for url in best_buy:
 for url in amazon:
     get_status(url, "a", "class", "a-button-text", headers=True)
 
-
-
+for url in gamestop:
+    get_status(url, "div", "class", "add-to-cart-buttons", headers=True)
---
+++ dump.py
@@ -0,0 +1,13 @@
+headergs = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/'
+              '*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    "Host": 'www.gamestop.com',
+    "Accept-Language": 'en-us',
+    "Accept-Encoding": 'gzip, deflate, br',
+    "Connection": 'keep-alive',
+    "DNT": '1',
+    "Sec-Fetch-Dest": 'document',
+    "Sec-Fetch-Mode": 'navigate',
+    "Sec-Fetch-Site": 'none',
+    "Upgrade-Insecure-Requests": '1',
+    "User-Agent":   'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'}
