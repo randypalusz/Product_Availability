5046aa0368a1444464f46a47576f6a993ee64999
randypalusz <ibanezguy998@gmail.com>
2020-04-18 02:46:00 -0400

    added email functionality

--- driver.py
+++ driver.py
@@ -1,10 +1,14 @@
-import requests
-# import numpy as np
-from bs4 import BeautifulSoup
-from selenium import webdriver
+import smtplib
+import ssl
 import sys
-import dump
 from collections import defaultdict
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+import requests
+from bs4 import BeautifulSoup
+
+import dump
 
 oos_strings = {"not available", "check stores", "sold out", "see all buying options"}
 header = {'User-Agent': 'Chrome/57.0.2987.110 '}
@@ -14,14 +18,28 @@ headerbb = {'User-Agent':   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
 headergs = {'User-Agent':   'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) '
                             'AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.4 '
                             'Safari/605.1.15'}
-stock_informer = {"https://www.stockinformer.com/checker-nintendo-switch-console"}
+stock_informer = ["https://www.stockinformer.com/checker-nintendo-switch-console"]
 retailer_list = []
 product_list = []
 status_list = []
 found = defaultdict(list)
 
+# Email stuff
+port = 465  # For SSL
+password = input("Type your password and press enter: ")
+conf_password = input("Confirm password: ")
+if password != conf_password:
+    print("Passwords not equal, ending program...")
+    sys.exit()
+
 
 def get_status(address, tag, attr, value, headers=False):
+    # Clear out global lists
+    retailer_list.clear()
+    product_list.clear()
+    status_list.clear()
+    found.clear()
+
     if headers:
         # use specific header for bestbuy
         if "bestbuy" in address:
@@ -53,7 +71,7 @@ def get_status(address, tag, attr, value, headers=False):
 
 def check_last_stock():
     # Product List goes from (0 -> max), (newest -> oldest)
-    # as long as first instance of unique product is in stock, product is in stock
+    # as long as first instance of unique product is in stock for given retailer, product is in stock
     for i in range(0, len(product_list)-1):
         product = product_list[i]
         status = status_list[i]
@@ -61,21 +79,24 @@ def check_last_stock():
         # if product not found in dictionary -> first instance of
         if not found[retailer].__contains__(product):
             print("Last '" + product + "' status at " + retailer + " is: " + status)
+            if "in" in status.lower():
+                send_email(product, status, retailer)
             found[retailer].append(product)
 
 
-
-# for url in target:
-#     get_status(url, "div", "data-test", "oosDeliveryOption", headers=False)
-#
-# for url in best_buy:
-#     get_status(url, "div", "class", "fulfillment-add-to-cart-button", headers=True)
-#
-# for url in amazon:
-#     get_status(url, "a", "class", "a-button-text", headers=True)
-
-# for url in gamestop:
-#     get_status(url, "div", "class", "add-to-cart-buttons", headers=True)
+def send_email(product, status, retailer):
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=ssl.create_default_context()) as server:
+        server.login("jswaggy789@gmail.com", password=password)
+        sender_email = "jswaggy789@gmail.com"
+        receiver_email = "ibanezguy998@gmail.com"
+        message = MIMEMultipart("alternative")
+        message["Subject"] = "Nintendo Switch In Stock - Randy Python Code"
+        message["From"] = sender_email
+        message["To"] = receiver_email
+        body = "Last '" + product + "' status at " + retailer + " is: " + status + "\nCheck here for details: " + \
+               stock_informer[0]
+        message.attach(MIMEText(body, "plain"))
+        server.sendmail(sender_email, receiver_email, message.as_string())
 
 for url in stock_informer:
     get_status(url, "table", "id", "TblFeed", headers=True)
