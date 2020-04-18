0a85d6a7742d055b07763a48d1791e746f9bfd22
randypalusz <ibanezguy998@gmail.com>
2020-04-18 19:34:24 -0400

    added potential multiple results in email with added links to purchase page

--- driver.py
+++ driver.py
@@ -21,8 +21,14 @@ oos_strings = {"not available", "check stores", "sold out", "see all buying opti
 header = {'User-Agent': 'Chrome/57.0.2987.110 '}
 stock_informer = ["https://www.stockinformer.com/checker-nintendo-switch-console"]
 retailer_list = []
+website_list = []
 product_list = []
 status_list = []
+price_list = []
+
+# product, retailer, status, price, website
+in_stock_list = []
+
 found = defaultdict(list)
 
 # Email stuff
@@ -34,11 +40,7 @@ password = args["password"]
 
 def get_status(address, tag, attr, value, headers=False):
     # Clear out global lists
-    retailer_list.clear()
-    product_list.clear()
-    status_list.clear()
-    found.clear()
-
+    clear_lists()
     if headers:
         get_request = requests.get(address, headers=header).text
     else:
@@ -54,7 +56,13 @@ def get_status(address, tag, attr, value, headers=False):
         product_line = row.find('span').contents[7].strip()
         product = product_line[0:product_line.rfind('-')].strip()
         product_list.append(product)
+        price = product_line[product_line.rfind('-')+1:].strip()
+        price_list.append(price)
+        website = "https://stockinformer.com/" + row.find('a').attrs['href']
+        website_list.append(website)
     check_last_stock()
+    if in_stock_list:
+        send_email()
 
 
 def check_last_stock():
@@ -64,25 +72,42 @@ def check_last_stock():
         product = product_list[i]
         status = status_list[i]
         retailer = retailer_list[i]
+        price = price_list[i]
+        website = website_list[i]
         # if product not found in dictionary -> first instance of
         if not found[retailer].__contains__(product):
-            print("Last '" + product + "' status at " + retailer + " is: " + status)
+            print("Most recent status of '" + product + "' at " + retailer + " is: " + status + " - " + price)
             if "in" in status.lower():
-                send_email(product, status, retailer)
+                in_stock_list.append([product, retailer, status, price, website])
             found[retailer].append(product)
 
 
-def send_email(product, status, retailer):
+def send_email():
     with smtplib.SMTP_SSL("smtp.gmail.com", port, context=ssl.create_default_context()) as server:
         server.login(sender_email, password=password)
         message = MIMEMultipart("alternative")
         message["Subject"] = "Nintendo Switch In Stock - Python Code"
         message["From"] = sender_email
         message["To"] = receiver_email
-        body = "Last '" + product + "' status at " + retailer + " is: " + status + "\nCheck here for details: " + \
-               stock_informer[0]
+        body = ""
+        for i in range(0, len(in_stock_list)):
+            body += "Most recent status of  '" + in_stock_list[i][0] + \
+                    "' at " + in_stock_list[i][1] + \
+                    " is: " + in_stock_list[i][2] + \
+                    " - " + in_stock_list[i][3] + \
+                    "\nCheck here for details: " + in_stock_list[i][4] + \
+                    "\n\n"
         message.attach(MIMEText(body, "plain"))
         server.sendmail(sender_email, receiver_email, message.as_string())
+
+
+def clear_lists():
+    retailer_list.clear()
+    website_list.clear()
+    product_list.clear()
+    status_list.clear()
+    price_list.clear()
+    found.clear()
 
 
 for url in stock_informer:
