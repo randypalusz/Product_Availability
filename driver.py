905c08da02aa761b59b7123a37a82cecc9b95c0f
randypalusz <ibanezguy998@gmail.com>
2020-04-18 00:29:36 -0400

    stockchecker website functionality added

--- driver.py
+++ driver.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 import sys
 import dump
+from collections import defaultdict
 
 oos_strings = {"not available", "check stores", "sold out", "see all buying options"}
 header = {'User-Agent': 'Chrome/57.0.2987.110 '}
@@ -13,15 +14,11 @@ headerbb = {'User-Agent':   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
 headergs = {'User-Agent':   'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) '
                             'AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.4 '
                             'Safari/605.1.15'}
-target = {"https://www.target.com/p/nintendo-switch-with-neon-blue-and-neon-red-joy-con/-/A-77464001",
-          "https://www.target.com/p/nintendo-switch-with-gray-joy-con/-/A-77464002"}
-best_buy = {"https://www.bestbuy.com/site/nintendo-switch-32gb-console-gray-joy-con/6364253.p?skuId=6364253",
-            "https://www.bestbuy.com/site/nintendo-switch-32gb-console-neon-red-neon-blue-joy-con/6364255.p?skuId=6364255"}
-amazon = {"https://www.amazon.com/Nintendo-Switch-Neon-Blue-Joy%E2%80%91/dp/B07VGRJDFY/ref=sr_1_2",
-          "https://www.amazon.com/Nintendo-Switch-Gray-Joy%E2%80%91-HAC-001/dp/B07VJRZ62R/ref=sr_1_2",
-          "https://www.amazon.com/Nintendo-Switch-Animal-Crossing-New-Horizons/dp/B084DDDNRP/ref=sr_1_5"}
-gamestop = {"https://www.gamestop.com",
-            "https://www.gamestop.com/video-games/switch/consoles/products/nintendo-switch-with-neon-blue-and-neon-red-joy-con/11095819.html"}
+stock_informer = {"https://www.stockinformer.com/checker-nintendo-switch-console"}
+retailer_list = []
+product_list = []
+status_list = []
+found = defaultdict(list)
 
 
 def get_status(address, tag, attr, value, headers=False):
@@ -37,24 +34,48 @@ def get_status(address, tag, attr, value, headers=False):
         get_request = requests.get(address).text
     doc = BeautifulSoup(get_request, 'html.parser')
     # print(doc.prettify())
-    rows = doc.find_all(tag, {attr: value}, limit=1)
+    rows_temp = doc.find(tag, {attr: value})
+    rows = rows_temp.find_all('tr', limit=10)
     for row in rows:
-        stock = row.getText().strip().lower()
-        # print(stock)
-        if stock in oos_strings:
-            print("Out of stock at: " + address)
-        else:
-            print("In stock at: " + address)
+        retailer = row.find('img').attrs['alt']
+        retailer_list.append(retailer)
+        status = row.find('span').contents[4].getText()
+        status_list.append(status)
+        product_line = row.find('span').contents[7].strip()
+        product = product_line[0:product_line.rfind('-')].strip()
+        product_list.append(product)
+    # print(product_list)
+    # print(status_list)
+    # print(retailer_list)
+    check_last_stock()
+    # print(found)
 
 
-for url in target:
-    get_status(url, "div", "data-test", "oosDeliveryOption", headers=False)
+def check_last_stock():
+    # Product List goes from (0 -> max), (newest -> oldest)
+    # as long as first instance of unique product is in stock, product is in stock
+    for i in range(0, len(product_list)-1):
+        product = product_list[i]
+        status = status_list[i]
+        retailer = retailer_list[i]
+        # if product not found in dictionary -> first instance of
+        if not found[retailer].__contains__(product):
+            print("Last '" + product + "' status at " + retailer + " is: " + status)
+            found[retailer].append(product)
 
-for url in best_buy:
-    get_status(url, "div", "class", "fulfillment-add-to-cart-button", headers=True)
 
-for url in amazon:
-    get_status(url, "a", "class", "a-button-text", headers=True)
+
+# for url in target:
+#     get_status(url, "div", "data-test", "oosDeliveryOption", headers=False)
 #
+# for url in best_buy:
+#     get_status(url, "div", "class", "fulfillment-add-to-cart-button", headers=True)
+#
+# for url in amazon:
+#     get_status(url, "a", "class", "a-button-text", headers=True)
+
 # for url in gamestop:
 #     get_status(url, "div", "class", "add-to-cart-buttons", headers=True)
+
+for url in stock_informer:
+    get_status(url, "table", "id", "TblFeed", headers=True)
--- dump.py
+++ dump.py
@@ -1,43 +1,15 @@
-headergs = {
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;'
-              'q=0.8,application/signed-exchange;v=b3;q=0.9',
-    "Host": 'www.gamestop.com',
-    "Cookie": '__cfduid=d33747bba7c1aa47533082e1b834d5bc71585464494; '
-              'dwac_a78eb5a11975e4c9cbc84f55ad=8Nbag-VF-xPyTXuCRbMqr8i3dyS_4EaN-X4%3D|dw-only|||USD|false|US'
-              '%2FCentral|true; cqcid=beORl4ohma4mvq5Be10v4beScj; sid=8Nbag-VF-xPyTXuCRbMqr8i3dyS_4EaN-X4; '
-              'dwanonymous_420142ceefb9f0c103b3815e84e9fcef=beORl4ohma4mvq5Be10v4beScj; __cq_dnt=0; dw_dnt=0; '
-              'dwsid=NMkHwjdPbDQrsu6FDndQ9QyoqmVt6tGqIIGSBhIKq1THPtmhY5iDkfyE6w5-hOkl_0CUASuRdUzYz9VOUijuyw==; '
-              'ak_bmsc=5BE82A53DC49A63B12B1F6F99CCC10E04056CEE4D96A0000AE44805EEFB3DC33'
-              '~plAwK0T6wHRBItk2OOQfqEsKK63vVPRVFV+wdj6a'
-              '/1Rzj27NzUWTXfd6EDDW9Hg1mn3kHSbgvWFj4OUCMN0zQcZwRGuWkJ9FfyXghz6v71uk'
-              '/HeJafajz0Sdbjuf7bMnYNw1ksaqh4kMVfed8sL7MXKNMRD4rXlviGe+8Dn++Jw/VCybBoAHKzP'
-              '+zpDxYID2OKD3aLcAqYncprvBwaFygfuMNTFs1BdgHAA065bNefk48=; '
-              'bm_sz=0411D6909E743BA4F977C44C6ED12E36~YAAQ5M5WQA7+PLJwAQAALUsMJQdANrHBFQv37Ivnab3OYXqNkZBrFF'
-              '+cVPKWQ4B5u05iThWBtWuH3gkVHNelrsKy4Fw9aSi1fb7d6EPayioG+BKAKFVF'
-              '/TWbhFSE7UjOKdZu9fyuoknZN3qFFUKrY6WHpHACKB3UtWszFhLiqLM8LbUUnA8XTckxKpRuZkgnOFw=; '
-              '_caid=302800d8-ebb8-4832-ae55-46a5d5888c6b; _cavisit=171250c491e|; '
-              'bm_mi=AC7E413768F16C2E2DBFED130461A45F'
-              '~VWyDto4OwfZ09faCeMqSb4AXSLJ2fetgQnXk88hQsdhQ0aOFljOLQhqR1h2gpRWISDJOBel'
-              '/4Mp3cFZo3XSROYJcaKbkXfSEJcmcsw6jgaOgvSR5Xu/0htFdguKh2XCaX3wny40FrjCMb3/51vyp1WV'
-              '/YKaKGBq1fSThIakppIurTBCC3ZSsAh2TahL7aQM73BKTHUvhfVk7csh3LnwxyJhqZ2IaxWiRp6EIsvFMznw=; '
-              'bm_sv=F427EAB5238D068CA4617916FC77B627~KVyNjE1d8kwUWo52OziMKYoqmwrMv6narmvL9agUG6B'
-              '+shJzljxfVkRZndMH32p93MywpOGNA8eNMzS5UDZkqi5YNV9JI1UWvp8l'
-              '/RIDq9uXIXHHLkuq9DFpkQ7hegk3pJ9fiIsxgTLJyqGx2nqGqccND35rjDgDMXONKAL4A0M=; '
-              '_abck=181F727B927EBB30DA36925ECE13E819~-1~YAAQzs5WQKDihgtxAQAAk3IOJQNDLPA3PTXJ3o8DrAszggm1ysVu'
-              '+zfTNdh2tHmcFsM7khUW6wLiqeP4NHmG+HtR4XWJc8MkkdKPMKUnHI8I+KLsy6pEhkijFV9XHtU0ojBcxd+P9NKwQIsIeSv7SL/Z'
-              '+kSPx+9Qwxww7sI1euKhaf40qifTos6tAam/y6b+YrtvlbsbOo40SG92rxCZuKJQ4t'
-              '/1ADEt4MY3cDZE5dF62ytt0BhzrkFkOfGF692iVZP4Xx0g9C6A8ukQLZRUCXQdCfRvpRPhLcF2aCdoXt9HrY6Jz5j8C4Ir0v'
-              '/UvOvbBPeMKdhwGVTZnlx2FeojynJOZyeDSQ==~-1~-1~-1',
-    "Cache-Control": 'max-age=0',
-    "Accept-Language": 'en-us',
-    "Accept-Encoding": 'gzip, deflate, br',
-    "Connection": 'keep-alive',
-    "DNT": '1',
-    "Sec-Fetch-Dest": 'document',
-    "Sec-Fetch-Mode": 'navigate',
-    "Sec-Fetch-Site": 'none',
-    "Sec-Fetch-User": '?1',
-    "Upgrade-Insecure-Requests": '1',
-    "User-Agent":   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-                    'AppleWebKit/537.36 (KHTML, like Gecko) '
-                    'Chrome/80.0.3987.149 Safari/537.36'}
+headergs = {'Host': 'www.gamestop.com',
+            'Connection': 'keep-alive',
+            'Upgrade-Insecure-Requests': '1',
+            'DNT': '1',
+            'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36',
+            'Sec-Fetch-Dest': 'document',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'Sec-Fetch-Site': 'same-origin',
+            'Sec-Fetch-Mode': 'navigate',
+            'Sec-Fetch-User': '?1',
+            'Referer': 'https://www.gamestop.com/',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'en-US,en;q=0.9,de-DE;q=0.8,de;q=0.7',
+            'Cookie': '__cfduid=d33747bba7c1aa47533082e1b834d5bc71585464494; dwac_a78eb5a11975e4c9cbc84f55ad=8Nbag-VF-xPyTXuCRbMqr8i3dyS_4EaN-X4%3D|dw-only|||USD|false|US%2FCentral|true; cqcid=beORl4ohma4mvq5Be10v4beScj; sid=8Nbag-VF-xPyTXuCRbMqr8i3dyS_4EaN-X4; dwanonymous_420142ceefb9f0c103b3815e84e9fcef=beORl4ohma4mvq5Be10v4beScj; __cq_dnt=0; dw_dnt=0; dwsid=NMkHwjdPbDQrsu6FDndQ9QyoqmVt6tGqIIGSBhIKq1THPtmhY5iDkfyE6w5-hOkl_0CUASuRdUzYz9VOUijuyw==; _caid=302800d8-ebb8-4832-ae55-46a5d5888c6b; bm_sz=68FE532BC7434DD146F367BE8E0D24C4~YAAQ3wHorLuc5xJxAQAAi1IzKQfl+XS85db4mSGNTLrRl0CjIqb6aZYRzOjTvOfF+zBl9LZSI77IyEpFWhsTQwPKc6A8PbUmPUvIWUW+ACJHjWfwAqK5y0IZf/G9t2dNRCgJ7OKdGqOGWLJyHCMsjwCp56fvtlJQvoMnjFibC4O6ZKekeaVRjS6NIk3M4olHtZs=; _abck=181F727B927EBB30DA36925ECE13E819~-1~YAAQ3wHorLyc5xJxAQAAi1IzKQN17U5aCUDvu4d43ACSGwDR0m3n3PKctyhmAAMizyV1uarNspLDvM/0WVElFg4wCESB0cKkYM81VBx1QzUIMKCJ+HPcfna8i7aFN+25nqpjpBHn2TJ7+Dp2sP1We9cenZ+o2Z5tw+O/93ksf4w0R70pb6Q/qBbIo5Q4wEULDk/suz3g8NQIvjDOzDl1HOALRmVML/sVgRQ10riMJX0QpsXVbZPbIcC7kjbnYxesfOozjPdEb/jxR9IyuY9wX6R9h9F/gd8+YMiRqiIuXulDVcjeCb6Oc5kgZtf8xl9ztbvSjYSelLudlBUvHV2pcWEq9dgmzw==~-1~-1~-1'
+            }
